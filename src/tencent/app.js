22b6f7a893fb6e48c025326e5bda4768b067ae8e
LiuKairui <tclkr@live.com>
2022-04-02 23:13:58 +0800

    fix: tencent_cloud

--- src/tencent/app.js
+++ src/tencent/app.js
@@ -1,6 +1,6 @@
 exports.main_handler = async (event, context) => {
   const t = new Date();
-  if (t.getUTCHours() + 8 >= 18) {
+  if (t.getUTCHours() + 8 < 18) {
     const morning = require('./morning');
     return morning();
   } else {
