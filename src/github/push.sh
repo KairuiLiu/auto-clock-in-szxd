df2eee7b89a37a2ee87631a9019ada48a90d28b9
LiuKairui <tclkr@live.com>
2022-05-20 13:52:10 +0800

    fix: github-action

--- src/github/push.sh
+++ src/github/push.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-remote_repo="https://${GITHUB_ACTOR}:${GH_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" # remote repo address
+remote_repo="https://${GITHUB_ACTOR}:$GH_TOKEN@github.com/${GITHUB_REPOSITORY}.git" # remote repo address
 
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "GitHub Actions"
 
-git add .
-git commit -m "update clockin log 😎"
-git push
+git add . && git commit -m "update clockin log 😎" && git push && curl \
+  --header 'Authorization: Bearer $GH_TOKEN' -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/dispatches \
+  -d '{"event_type":"reclockin"}'
